0b17d3f15185fe22c4608725f90bd9cf0103305d
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2024-09-26 18:32:20 -0600

    Cambios en el repositorio automaticos

---
+++ ejerciciosPython/fahrenheit_while.py
@@ -0,0 +1,6 @@
+#Convertir grados fahrenheit a celcius
+temp = 32
+print("f°        c°")
+while temp < 73:
+    print(temp, "       ", int((temp-32)*5/9))
+    temp = temp + 1
---
+++ ejerciciosPython/pruebas.py
@@ -0,0 +1,69 @@
+# Determina el valor de x al final del siguiente código:
+# x = 1
+# i = 0
+# while i < 4:
+#     x = x * 2
+#     i += 1
+#     print(x)
+# print(x)
+
+# Pregunta 2 Al final de este código: El valor de n es:
+# x = 48
+# y = 8 
+# n = 0
+# while x > 0:
+#     x = x - y
+#     n = n + 1
+
+# print(n)
+
+#Pregunta 3 Considerando las variables del siguiente código: El output del mismo es equivalente a imprimir:
+# a = 5
+# b = 8
+# r = 0
+# while a > 0:
+#     r = r + b
+#     a = a - 1
+#     print(r)
+
+#Determina una opción que entregue el mismo resultado que el código a continuación:}
+# a = 4
+# b = 3
+# r = b
+
+# while a > 1:
+#     a = a -1
+#     b2 = b
+#     r2 = 0
+#     while b2 > 0:
+#         r2 = r2 + r
+#         b2 = b2 -1 
+#     r = r2
+# print(r)
+
+# a = 2
+# for i in range(1,4):
+#     a = i ** a
+# print(a)
+
+
+# saludo = "Hola, mundo"
+# vuelta = 0
+# for i in range(3,12):
+#     vuelta += 1
+#     print(saludo, vuelta)
+
+# a = 0
+# for i in range(3):
+#     a += i
+# print(a)
+
+# numero = 1
+# while numero <= 5:
+#     print(numero, numero**2)
+
+# vueltaj = 1
+# vueltai = 1
+for i in range (1,101):
+    for j in range(1,101):
+        print(i, j)
