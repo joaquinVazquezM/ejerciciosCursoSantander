c8e6d684c1520ab122937957b0bfbed2fd8bb646
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2024-10-10 21:51:16 -0600

    Resolución de conflictos

--- ejerciciosPython/fahrenheit_for.py
+++
@@ -1,4 +0,0 @@
-#Tabla de conversión de f° a c° de 0 hasta 20
-print("f°       c°")
-for temp in range(21):
-    print(temp,"      ",int((temp-32)*5/9))
--- ejerciciosPython/fahrenheit_while.py
+++
@@ -1,6 +0,0 @@
-#Convertir grados fahrenheit a celcius
-temp = 32
-print("f°        c°")
-while temp < 73:
-    print(temp, "       ", int((temp-32)*5/9))
-    temp = temp + 1
