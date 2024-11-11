6a50051bedf77629772370253106dff6999dc67e
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2024-11-10 22:45:44 -0600

    DocsString y funciones con numero variables de argumentos

--- funciones.py
+++ funciones.py
@@ -75,3 +75,48 @@ funcion()
 funcion_2()
 print(f"Imprime la variable goblal \n {variable_global}")
 #print(variable_local) - Esto es un error
+
+# DocsString
+
+print("Cálcuola el area de un rectangulo")
+
+def area_rectangulo(base, altura):
+    """
+    Calcula el área de un triangulo.
+
+    Args:
+        base(float): La base del rectangulo
+        altura(float): La altura del recutangulo
+
+    Returns:
+        float: El área del rectangulo
+    """
+    return base * altura
+
+resultado_area_rectangulo = area_rectangulo(5 , 5)
+print(f"El área del rectangulo es: {resultado_area_rectangulo}" )
+
+# Funciones con numero variable de argumentos
+
+def suma_variable(*numeros):
+    total = 0
+    for numero in numeros:
+        total += numero
+    return total
+
+print(suma_variable(3,4,5))
+print(suma_variable(3,4,5,45,5))
+
+#Ejemplo de funciones con numero variabes de argumentos
+
+print("Promedio")
+
+def promedio_varible(*numeros):
+    total = 0
+    for numero in numeros:
+        total += numero
+    return total / len(numeros) if numeros else 0
+
+
+print(f"El promedio es:\t {promedio_varible(10, 10, 8, 9, 7)}")
+
