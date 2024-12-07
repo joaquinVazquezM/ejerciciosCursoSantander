06cc9031d4d667cbaf9b663178394f563c2b98e6
joaquinVazquez <joaquin.vazquez@unisa.edu.com>
2024-12-06 21:37:58 -0600

    Ejercicios de booleanos

---
+++ booleanos.py
@@ -0,0 +1,8 @@
+# Definir un valor booleano que indica si la tienda está abierta
+tienda_abierta = True
+
+# Verificar si la tienda está abierta
+if tienda_abierta:
+    print("La tienda está abierta.")
+else:
+    print("La tienda está cerrada.")
