6772a11e875ffd043223da0efb3d8f25dd51adc6
joaquinVazquez <joaquin.vazquez@unisa.edu.com>
2024-12-29 08:58:00 -0600

    Organización de modulos

---
+++ crar_utilizar_modulos.py
@@ -0,0 +1,5 @@
+import mi_modulo
+
+mi_modulo.saludar("Joaquin")
+resultado = mi_modulo.calcular_suma(5 ,3)
+print(resultado)
---
+++ mi_modulo.py
@@ -0,0 +1,8 @@
+#mi_modulo.py
+def saludar(nombre):
+    print(f"Hola, {nombre}!")
+
+def calcular_suma(a, b):
+    return a + b
+
+
